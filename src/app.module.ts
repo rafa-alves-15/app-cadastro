c783fe38574635f55f35daa87e35dd24c8830f97
rafa-alves-15 <git@github.com>
2022-01-04 11:34:37 -0300

    feat: code refactoring completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ProductsModule } from './product/products.module';
 import { UsersModule } from './users/users.module';
-import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [UsersModule, ProductsModule],
