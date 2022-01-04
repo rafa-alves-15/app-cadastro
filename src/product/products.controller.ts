cc19d9b193910ec31f7f4449df5ecc84efbbd161
rafa-alves-15 <git@github.com>
2022-01-04 11:26:43 -0300

    feat: created update and edit

--- src/product/products.controller.ts
+++ src/product/products.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateProductsDto } from './dto/create-products.dto';
-import { Products } from './products.model';
+import { Products, ProductStatus } from './products.model';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -11,9 +11,31 @@ export class ProductsController {
     getAllProducts(): Products[] {
       return this.productsService.getAllProducts();
     }
+
+    @Get('/:id')
+    getProductsById(@Param('id') id: string): Products {
+      return this.productsService.getProductsById(id);
+    }
+
     @Post()
     createProducts(@Body() createProductsDto: CreateProductsDto): Products {
       return this.productsService.createProducts(createProductsDto);
-    }    
+    }  
+    
+    @Delete('/:id')
+    DeleteProducts(@Param('id') id: string): void {
+      return this.productsService.DeleteProducts(id);
+    }
+
+    @Patch('/:id/edit')
+    updateProducts(
+      @Param('id') id: string,
+      @Body('name') name: string,
+      @Body('brand') brand: string,
+      @Body('price') price: number,
+      @Body('status') status: ProductStatus,
+    ): Products {
+      return this.productsService.updateProducts(id, name, brand, price, status);
+    }
   }
 
--- src/product/products.service.ts
+++ src/product/products.service.ts
@@ -10,6 +10,11 @@ export class ProductsService {
   getAllProducts() {
     return this.products;
   }
+
+  getProductsById(id: string): Products {
+    return this.products.find((product) => product.id === id)
+  }
+
   createProducts(createProductsDto: CreateProductsDto): Products {
     const { name, brand, price} = createProductsDto
 
@@ -24,4 +29,18 @@ export class ProductsService {
     this.products.push(product);
     return product
   }
+
+  DeleteProducts(id: string): void {
+    this.products = this.products.filter((product) => product.id !== id)
+  }
+
+  updateProducts(id: string, name: string, brand: string, price: number, status: ProductStatus) {
+    const products = this.getProductsById(id);
+    products.name = name;
+    products.brand = brand;
+    products.price = price;
+    products.status = status;
+    return products
+  }
+
 }
