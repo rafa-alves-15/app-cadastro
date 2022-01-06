7ab43eaf4a9db619b2cb3b8f254956f11b5b613a
rafa-alves-15 <git@github.com>
2022-01-06 14:24:57 -0300

    feat: search filter creation

--- src/product/dto/get-products-filter.dto.ts
+++ src/product/dto/get-products-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { ProductStatus } from "../products.model";
 
 export class GetProductsFilterDto {
+  
+  @IsOptional()
+  @IsEnum(ProductStatus)
   status?: ProductStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
