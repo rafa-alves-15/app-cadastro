d5432eabb98a5aa594dcc3e8af30bf99714c45f7
rafa-alves-15 <git@github.com>
2022-01-07 11:55:24 -0300

    fix: import from the ProductsRepository

--- src/product/products.module.ts
+++ src/product/products.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsController } from './products.controller';
+import { ProductsRepository } from './products.repository';
 import { ProductsService } from './products.service';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([ProductsRepository])],
   controllers: [ProductsController],
   providers: [ProductsService]
 })
